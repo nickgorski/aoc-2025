02cddf8214ae0fd098ca4bde25c18cc643df28cf
Nick Gorski <ngorski@umich.edu>
2025-12-08 08:43:17 -0800

    Day 04 part 2 solution.

---
+++ 04/day_04.py
@@ -0,0 +1,37 @@
+import sys
+
+
+class Model:
+    def __init__(self, filename):
+        with open(filename, 'r') as in_file:
+            lines = [x.strip() for x in in_file.readlines()]
+        self.width, self.height = len(lines[0]), len(lines)
+        self.m = [['.' for i in range(self.width)] for j in range(self.height)]
+        
+        for j in range(self.height):
+            for i in range(self.width):
+                self.m[j][i] = lines[j][i]
+
+    def IsValidPos(self, j, i):
+        return j >= 0 and j < self.height and i >= 0 and i < self.width
+
+    def GetAllAdjacents(self, j, i):
+        adjacents = []
+        for y in [-1, 0, 1]:
+            for x in [-1, 0, 1]:
+                if self.IsValidPos(j + y, i + x) and (y != 0 or x != 0):
+                    adjacents.append(self.m[j + y][i + x])
+        return adjacents
+
+    def GetCell(self, j, i):
+        return self.m[j][i]
+
+
+if __name__ == '__main__':
+    m = Model(sys.argv[1])
+    count = 0
+    for j in range(m.height):
+        for i in range(m.width):
+            if m.GetCell(j, i) == '@' and m.GetAllAdjacents(j, i).count('@') < 4:
+                count += 1
+    print(count)
